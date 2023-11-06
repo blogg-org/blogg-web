15427ca00755e6fdc1d08d627f6a63a7c0e023d6
RameshNeupane <neupaneramesh555@gmail.com>
2023-11-06 18:37:59 +0545

    add pages

---
+++ src/pages/EditPost.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditPost: React.FC = () => {
+    return <div>EditPost</div>;
+};
+
+export default EditPost;
---
+++ src/pages/Post.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Post: React.FC = () => {
+    return <div>Post</div>;
+};
+
+export default Post;
