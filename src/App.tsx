b8a750b410fbadcca25390bd6dadbaa6d60d231e
RameshNeupane <neupaneramesh555@gmail.com>
2023-11-06 18:38:13 +0545

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
+import { Header, Footer, Container } from "@components/index";
 import React from "react";
+import { Outlet } from "react-router-dom";
 
 const App: React.FC = () => {
-    return <div>App</div>;
+    return (
+        <div className="relative min-w-screen min-h-screen flex flex-col justify-between">
+            <Header />
+            <main className="py-6">
+                <Container>
+                    <Outlet />
+                </Container>
+            </main>
+            <Footer />
+        </div>
+    );
 };
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        alias: {
+            "@assets": path.resolve(__dirname, "./src/assets/"),
+            "@components": path.resolve(__dirname, "./src/components/"),
+            "@pages": path.resolve(__dirname, "./src/pages/"),
+        },
+    },
 });
