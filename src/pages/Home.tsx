123c50db0b35006d463a7de45674ec119c5af967
RameshNeupane <neupaneramesh555@gmail.com>
2023-11-22 16:07:48 +0545

    update app

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home: React.FC = () => {
-    return <div>Home</div>;
+    return <div className=" font-bold text-2xl text-center">Home</div>;
 };
 
 export default Home;
